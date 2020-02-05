ea52e8efc0f70379eb3641f2132a5dd4cbd7e629
Teerth Patel <teerth1205@gmail.com>
2020-02-05 22:32:57 +0000

    Done.

---
+++ lib/user.rb
@@ -0,0 +1,19 @@
+class User
+  def first_name=(name)
+    @first = name 
+  end 
+  
+  def first_name
+    @first
+  end 
+  
+  def last_name=(name)
+    @first = name 
+  end 
+  
+  def last_name
+    @first
+  end 
+  
+end 
+  
